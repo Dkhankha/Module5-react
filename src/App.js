044430525fe5ec23039e1ea7a8f6e4c61b2e2c1c
Dkhankha <daniyal64k@gmail.com>
2023-11-25 01:27:02 +0530

    cart button done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
+  const [showCart, setShowCart] = useState(false)
+  
+  const showCartHandler = () => {
+   setShowCart(true);
+  }
+  
+  const closecarthandler = () => {
+    setShowCart(false)
+  }
+
   return (
     <div>
-      <Cart/>
-      <Header/>
+      {showCart && <Cart onClose={closecarthandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <Meals/>
     </div>
   );
